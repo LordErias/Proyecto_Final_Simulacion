ee1d95a925997d58af46dda3cdd61f97e0e5f09e
oswaldomercado26 <oswaldo.mercado7383@alumnos.udg.mx>
2021-06-25 08:13:18 -0500

    ventanas

---
+++ Proyecto_Final/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            spawnNewEnemy();
+        }
+    }
+
+    void spawnNewEnemy(){
+        int randomNumber = Mathf.RoundToInt(UnityEngine.Random.Range( 0, transform.childCount));
+        GameObject go = Instantiate(enemyPrefab, transform.GetChild(randomNumber).transform.position, Quaternion.identity);
+        go.transform.GetComponent<EnemyMovement>().wavepointIndex = randomNumber;
+    }
+
+}
