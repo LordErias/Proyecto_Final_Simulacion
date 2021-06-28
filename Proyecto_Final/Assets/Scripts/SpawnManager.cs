2beb0c0713d2aa8c38a3e748ca3791063e2cae0b
Luis Erick de Jesus Vazquez Gutierrez <lvazquez@lumston.com>
2021-06-28 03:42:35 -0500

    waypoint y spawners

--- Proyecto_Final/Assets/Scripts/SpawnManager.cs
+++ Proyecto_Final/Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
 
---
+++ actualizacion juego/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public float timeBetweenWaves = 10f;
+    public float timeBeteenEnemies = 1f;
+    public float countdownWaves = 2f;
+    public float countdownEnemies = 2f;
+
+    private int waveNumber = 1;
+    public GameObject spawners;
+    private GameObject[] enemies;
+    private Stack<int> enemiesStack;
+    private int EnemiesLeft;
+    public bool onWave = false;
+    
+    void Start()
+    {
+        enemiesStack = new Stack<int>();
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        EnemiesLeft = 0;
+    }
+
+    void Update()
+    {
+        if (countdownWaves <= 0)
+        {
+            countdownWaves = timeBetweenWaves;
+        }
+        if (enemies.Length < 1 && countdownWaves <= 0)
+        {
+            spawnWave();
+            // onWave = true;
+        }
+        if (countdownEnemies <= 0 && EnemiesLeft > 0 )
+        {
+            spawnEnemy();
+            EnemiesLeft = enemiesStack.Pop();
+            if (EnemiesLeft == 0)
+            {
+                // onWave = false;
+            }
+            countdownEnemies = timeBeteenEnemies;
+        }
+
+
+        countdownEnemies -= Time.deltaTime;
+        countdownWaves -= Time.deltaTime;
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    void spawnWave(){
+        int enemyCount = waveNumber * waveNumber + 1;
+        print(enemyCount);
+        for (int i = 0; i < enemyCount; i++)
+        {
+            enemiesStack.Push(i);
+        }
+        EnemiesLeft = enemiesStack.Peek();
+        waveNumber ++;
+    }
+    void spawnEnemy(){
+        spawners.GetComponent<SpawnManager>().spawnNewEnemy();
+    }
+}
