537bc7c3241f7749163e19c7e6f2dedb2329ed07
oswaldomercado26 <oswaldo.mercado7383@alumnos.udg.mx>
2021-06-30 15:04:51 -0500

    movimientos de objeto vr

---
+++ actualizacion juego/Assets/Scripts/Grap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grap : MonoBehaviour
+{
+    public bool canGrap=false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("i can grap");
+            canGrap = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        canGrap = false;
+    }
+}
